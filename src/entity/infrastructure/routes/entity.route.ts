580890e24d84b80bc3977b5ec3f064d50e34762c
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-07 20:20:05 -0300

    chore: comment route until fix it

--- src/entity/infrastructure/routes/entity.route.ts
+++ src/entity/infrastructure/routes/entity.route.ts
@@ -14,9 +14,9 @@ class EntityRoute {
     this.router.get('/api/entities/:id', async (req: FastifyRequest<{ Params: { id: string } }>, res) => {
       await this.handler.FindByID(req, res)
     })
-    this.router.get('/api/entities/:id', async (req: FastifyRequest<{ Querystring: Record<string, string> }>, res) => {
-      await this.handler.FindByName(req, res)
-    })
+    // this.router.get('/api/entities', async (req: FastifyRequest<{ Querystring: Record<string, string> }>, res) => {
+    //   await this.handler.FindByName(req, res)
+    // })
     this.router.post('/api/entities', async (req, res) => {
       await this.handler.Register(req, res)
     })
