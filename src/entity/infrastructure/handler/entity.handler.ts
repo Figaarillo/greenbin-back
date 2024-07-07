d3456665a727566fe343c81a652c3d5ed8492bc4
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-07 18:39:40 -0300

    feat: create handler to list all entities

--- src/entity/infrastructure/handler/entity.handler.ts
+++ src/entity/infrastructure/handler/entity.handler.ts
@@ -1,7 +1,9 @@
+import ListEntitiesUseCase from '@entity/aplication/usecases/list.usecase'
 import RegisterEntityUseCase from '@entity/aplication/usecases/register.usecase'
 import type EntityPayload from '@entity/domain/payloads/entity.payload'
 import type EntityRepository from '@entity/domain/repositories/entity.repository'
 import HandleHTTPResponse from '@shared/utils/http.response'
+import { GetPaginationParams } from '@shared/utils/http.utils'
 import { type FastifyReply, type FastifyRequest } from 'fastify'
 import RegisterEntityDTO from '../dtos/register-entity.dto'
 import SchemaValidator from '../middlewares/zod-schema-validator.middleware'
@@ -9,6 +11,19 @@ import SchemaValidator from '../middlewares/zod-schema-validator.middleware'
 class EntityHandler {
   constructor(private readonly repository: EntityRepository) {
     this.repository = repository
+  }
+
+  async List(req: FastifyRequest<{ Querystring: Record<string, string> }>, res: FastifyReply): Promise<void> {
+    try {
+      const { offset, limit } = GetPaginationParams(req)
+
+      const listEntities = new ListEntitiesUseCase(this.repository)
+      const entities = await listEntities.exec(offset, limit)
+
+      HandleHTTPResponse.OK(res, 'Entities retrieved successfully', entities)
+    } catch (error) {
+      res.status(500).send(error)
+    }
   }
 
   async Register(req: FastifyRequest, res: FastifyReply): Promise<void> {
