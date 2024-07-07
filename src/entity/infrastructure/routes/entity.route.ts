06b01bc7ac93347587ad6590141b3481b368b147
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-07 18:55:01 -0300

    feat: add routes to get by id, to get by name, and to list all entities

--- src/entity/infrastructure/routes/entity.route.ts
+++ src/entity/infrastructure/routes/entity.route.ts
@@ -1,4 +1,4 @@
-import { type FastifyInstance } from 'fastify'
+import { type FastifyInstance, type FastifyRequest } from 'fastify'
 import type EntityHandler from '../handler/entity.handler'
 
 class EntityRoute {
@@ -8,6 +8,15 @@ class EntityRoute {
   ) {}
 
   setupRoutes(): void {
+    this.router.get('/api/entities', async (req: FastifyRequest<{ Querystring: Record<string, string> }>, res) => {
+      await this.handler.List(req, res)
+    })
+    this.router.get('/api/entities/:id', async (req: FastifyRequest<{ Params: { id: string } }>, res) => {
+      await this.handler.FindByID(req, res)
+    })
+    this.router.get('/api/entities/:id', async (req: FastifyRequest<{ Querystring: Record<string, string> }>, res) => {
+      await this.handler.FindByName(req, res)
+    })
     this.router.post('/api/entities', async (req, res) => {
       await this.handler.Register(req, res)
     })
