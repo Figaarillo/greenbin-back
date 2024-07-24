354b10f590a66e4d38f5625fa3e7a7838900a1d6
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-24 10:29:49 -0300

    feat: implement reposiotry

--- src/entity/domain/repositories/entity.repository.ts
+++ src/entity/domain/repositories/entity.repository.ts
@@ -1,12 +1,11 @@
 import type Nullable from '@shared/domain/types/nullable.type'
 import type EntityEntity from '../entities/entity.entity'
-import type EntityPayload from '../payloads/entity.payload'
 
 interface EntityRepository {
   list: (offset: number, limit: number) => Promise<Nullable<EntityEntity[]>>
   find: (property: Record<string, string>) => Promise<Nullable<EntityEntity>>
   save: (entity: EntityEntity) => Promise<Nullable<EntityEntity>>
-  update: (id: string, entity: EntityPayload) => Promise<Nullable<EntityEntity>>
+  update: (id: string, description: string) => Promise<Nullable<EntityEntity>>
   delete: (id: string) => Promise<void>
 }
 
--- src/entity/infrastructure/repositories/mikro-orm/entity.mikroorm.repository.ts
+++ src/entity/infrastructure/repositories/mikro-orm/entity.mikroorm.repository.ts
@@ -1,5 +1,4 @@
-import type EntityEntity from '@entity/domain/entities/entity.entity'
-import type EntityPayload from '@entity/domain/payloads/entity.payload'
+import EntityEntity from '@entity/domain/entities/entity.entity'
 import type EntityRepository from '@entity/domain/repositories/entity.repository'
 import { type EntityManager } from '@mikro-orm/postgresql'
 import type Nullable from '@shared/domain/types/nullable.type'
@@ -12,11 +11,37 @@ class EntityMikroORMRepository implements EntityRepository {
     this.em = this.db.em.fork()
   }
 
-  List!: (offset: number, limit: number) => Promise<Nullable<EntityEntity[]>>
-  Find!: (property: Record<string, string>) => Promise<Nullable<EntityEntity>>
-  Save!: (entity: EntityEntity) => Promise<Nullable<EntityEntity>>
-  Update!: (id: string, entity: EntityPayload) => Promise<Nullable<EntityEntity>>
-  Delete!: (id: string) => Promise<void>
+  async list(offset: number, limit: number): Promise<Nullable<EntityEntity[]>> {
+    return await this.em.find(EntityEntity, {}, { limit, offset })
+  }
+
+  async find(property: Record<string, string>): Promise<Nullable<EntityEntity>> {
+    return await this.em.findOne(EntityEntity, property)
+  }
+
+  async save(entity: EntityEntity): Promise<Nullable<EntityEntity>> {
+    const newEntity = this.em.create(EntityEntity, entity)
+    await this.em.persist(newEntity).flush()
+
+    return newEntity
+  }
+
+  async update(id: string, description: string): Promise<Nullable<EntityEntity>> {
+    const entity = this.em.getReference(EntityEntity, id)
+    if (entity == null) return null
+
+    entity.update(description)
+    await this.em.flush()
+
+    return entity
+  }
+
+  async delete(id: string): Promise<void> {
+    const entity = this.em.getReference(EntityEntity, id)
+    if (entity == null) throw new Error('Entity not found')
+
+    await this.em.remove(entity).flush()
+  }
 }
 
 export default EntityMikroORMRepository
