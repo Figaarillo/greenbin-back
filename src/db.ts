2e9f87cfe4e96164f0c60e0c585d0c725c53ea5f
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-24 10:07:03 -0300

    feat: create init for mikro orm

---
+++ src/db.ts
@@ -0,0 +1,29 @@
+import EntityEntity from '@entity/domain/entities/entity.entity'
+import { MikroORM, type EntityManager } from '@mikro-orm/core'
+import { type EntityRepository, type Options } from '@mikro-orm/postgresql'
+
+export interface Services {
+  orm: MikroORM
+  em: EntityManager
+  entity: EntityRepository<EntityEntity>
+}
+
+let cache: Services
+
+async function initMikroORM(options?: Options): Promise<Services> {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (!cache) {
+    const orm = await MikroORM.init({
+      ...options
+    })
+    cache = {
+      orm,
+      em: orm.em,
+      entity: orm.em.getRepository(EntityEntity)
+    }
+  }
+
+  return cache
+}
+
+export default initMikroORM
