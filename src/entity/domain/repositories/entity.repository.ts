f4d1aa6fa21a8a0b43d35870c50703e31b2946c5
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-24 10:15:42 -0300

    feat: modify capitalize of methods

--- src/entity/domain/repositories/entity.repository.ts
+++ src/entity/domain/repositories/entity.repository.ts
@@ -3,11 +3,11 @@ import type EntityEntity from '../entities/entity.entity'
 import type EntityPayload from '../payloads/entity.payload'
 
 interface EntityRepository {
-  List: (offset: number, limit: number) => Promise<Nullable<EntityEntity[]>>
-  Find: (property: Record<string, string>) => Promise<Nullable<EntityEntity>>
-  Save: (entity: EntityEntity) => Promise<Nullable<EntityEntity>>
-  Update: (id: string, entity: EntityPayload) => Promise<Nullable<EntityEntity>>
-  Delete: (id: string) => Promise<void>
+  list: (offset: number, limit: number) => Promise<Nullable<EntityEntity[]>>
+  find: (property: Record<string, string>) => Promise<Nullable<EntityEntity>>
+  save: (entity: EntityEntity) => Promise<Nullable<EntityEntity>>
+  update: (id: string, entity: EntityPayload) => Promise<Nullable<EntityEntity>>
+  delete: (id: string) => Promise<void>
 }
 
 export default EntityRepository
