80c38fd4ed29e20ab381740228c17f6356a5b66e
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-24 19:23:07 -0300

    feat: modify bootstrap for entity

--- src/entity/entity.bootstrap.ts
+++ src/entity/entity.bootstrap.ts
@@ -2,9 +2,8 @@ import { type FastifyInstance } from 'fastify'
 import { type Services } from 'src/db'
 import type EntityRepository from './domain/repositories/entity.repository'
 import EntityHandler from './infrastructure/handler/entity.handler'
-import EntityTypeormRepository from './infrastructure/repositories/typeorm/entity.typeorm.repository'
-import EntityRoute from './infrastructure/routes/entity.route'
 import EntityMikroORMRepository from './infrastructure/repositories/mikro-orm/entity.mikroorm.repository'
+import EntityRoute from './infrastructure/routes/entity.route'
 
 async function bootstrapEntity(router: FastifyInstance, db: Services): Promise<void> {
   const repository: EntityRepository = new EntityMikroORMRepository(db)
