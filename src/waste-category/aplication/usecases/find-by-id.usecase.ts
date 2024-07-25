5c2f32a3d6de757b9c7cc265d9bbc2593b1ea128
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-25 19:19:51 -0300

    feat: create use case to list find category by id

---
+++ src/waste-category/aplication/usecases/find-by-id.usecase.ts
@@ -0,0 +1,20 @@
+import type WasteCategoryEntity from '../../domain/entities/waste-category.entity'
+import ErrorCategoryNotFound from '../../domain/errors/category-not-found.error'
+import type WasteCategoryRepository from '../../domain/repositories/waste-category.repository'
+
+class FindCategoryByIDUseCase {
+  constructor(private readonly repository: WasteCategoryRepository) {
+    this.repository = repository
+  }
+
+  async exec(id: string): Promise<WasteCategoryEntity> {
+    const categoryFound = await this.repository.find({ id })
+    if (categoryFound == null) {
+      throw new ErrorCategoryNotFound(`Cannont find category with id: ${id} when get category by id`)
+    }
+
+    return categoryFound
+  }
+}
+
+export default FindCategoryByIDUseCase
