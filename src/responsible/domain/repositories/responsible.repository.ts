5a3bfe52a0e93d2ebb767a7584d06ba51bcf5c5d
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-26 13:05:55 -0300

    chore: update payload of update method

--- src/responsible/domain/repositories/responsible.repository.ts
+++ src/responsible/domain/repositories/responsible.repository.ts
@@ -1,11 +1,12 @@
 import type Nullable from '../../../shared/domain/types/nullable.type'
 import type ResponsibleResponsible from '../entities/responsible.entity'
+import type ResponsibleUpdatePayload from '../payloads/responsible.update.payload'
 
 interface ResponsibleRepository {
   list: (offset: number, limit: number) => Promise<Nullable<ResponsibleResponsible[]>>
   find: (property: Record<string, string>) => Promise<Nullable<ResponsibleResponsible>>
   save: (responsible: ResponsibleResponsible) => Promise<Nullable<ResponsibleResponsible>>
-  update: (id: string, username: string, phoneNumber: number) => Promise<Nullable<ResponsibleResponsible>>
+  update: (id: string, payload: ResponsibleUpdatePayload) => Promise<Nullable<ResponsibleResponsible>>
   delete: (id: string) => Promise<void>
 }
 
