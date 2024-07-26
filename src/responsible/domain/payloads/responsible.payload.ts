e60b7867290319eca1a3d4757d299d49bfee0f5f
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-26 12:17:16 -0300

    feat: create payload of reponsible

---
+++ src/responsible/domain/payloads/responsible.payload.ts
@@ -0,0 +1,11 @@
+interface ResponsiblePayload {
+  firstname: string
+  lastname: string
+  username: string
+  email: string
+  password: string
+  dni: number
+  phoneNumber: number
+}
+
+export default ResponsiblePayload
