40ca9a286f48d80674c0db1fa4b912ceaece24af
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-26 17:32:17 -0300

    chore: correct name of entity

--- src/responsible/domain/repositories/responsible.repository.ts
+++ src/responsible/domain/repositories/responsible.repository.ts
@@ -1,12 +1,12 @@
 import type Nullable from '../../../shared/domain/types/nullable.type'
-import type ResponsibleResponsible from '../entities/responsible.entity'
+import type ResponsibleEntity from '../entities/responsible.entity'
 import type ResponsibleUpdatePayload from '../payloads/responsible.update.payload'
 
 interface ResponsibleRepository {
-  list: (offset: number, limit: number) => Promise<Nullable<ResponsibleResponsible[]>>
-  find: (property: Record<string, string>) => Promise<Nullable<ResponsibleResponsible>>
-  save: (responsible: ResponsibleResponsible) => Promise<Nullable<ResponsibleResponsible>>
-  update: (id: string, payload: ResponsibleUpdatePayload) => Promise<Nullable<ResponsibleResponsible>>
+  list: (offset: number, limit: number) => Promise<Nullable<ResponsibleEntity[]>>
+  find: (property: Record<string, string>) => Promise<Nullable<ResponsibleEntity>>
+  save: (responsible: ResponsibleEntity) => Promise<Nullable<ResponsibleEntity>>
+  update: (id: string, payload: ResponsibleUpdatePayload) => Promise<Nullable<ResponsibleEntity>>
   delete: (id: string) => Promise<void>
 }
 
