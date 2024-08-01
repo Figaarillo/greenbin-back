00ed6a12c744db9f2b63e5f4f8d8f33966d2b62d
Figaarillo <axel.leonardi.22@gmail.com>
2024-08-01 17:40:59 -0300

    chore: replace env vars in test setup

--- src/shared/test/test.setup.ts
+++ src/shared/test/test.setup.ts
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv'
 import { type FastifyInstance } from 'fastify'
 import { afterAll, afterEach, beforeAll, beforeEach } from 'vitest'
 import bootstrapApp from '../../app'
+import EnvVar from '../config/env-var.config'
 
 dotenv.config()
 
@@ -12,13 +13,13 @@ let app: FastifyInstance
 
 beforeAll(async () => {
   const { app: fastify, db } = await bootstrapApp(0, {
-    dbName: process.env.TEST_DATABASE_NAME,
-    user: process.env.TEST_DATABASE_USER,
-    host: process.env.TEST_DATABASE_HOST,
-    password: process.env.TEST_DATABASE_PASS,
+    dbName: EnvVar.testDatabase.name,
+    user: EnvVar.testDatabase.user,
+    password: EnvVar.testDatabase.password,
+    host: EnvVar.testDatabase.host,
+    port: EnvVar.testDatabase.port,
     entities: ['./dist/**/*.entity.js'],
     entitiesTs: ['./src/**/*.entity.ts'],
-    port: Number(process.env.TEST_DATABASE_PORT),
     metadataProvider: TsMorphMetadataProvider,
     debug: false,
     dynamicImportProvider: async id => await import(id)
