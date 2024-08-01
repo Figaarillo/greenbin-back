01bd9f3e8a3b12a7a7ff553d81ece2774d86a77e
Figaarillo <axel.leonardi.22@gmail.com>
2024-08-01 01:37:20 -0300

    feat: test udpate an entity and get updated entity

--- src/entity/test/entity.test.ts
+++ src/entity/test/entity.test.ts
@@ -2,10 +2,10 @@
 import { describe, expect, it } from 'vitest'
 import { app } from '../../shared/test/test.setup'
 
-let id: string
+let entityId: string
 
-describe('Entity API', () => {
-  it('should create a new entity', async () => {
+describe('Entity API Integration Tests', () => {
+  it('should create a new entity successfully', async () => {
     const response = await app.inject({
       method: 'POST',
       url: '/api/entity',
@@ -23,17 +23,41 @@ describe('Entity API', () => {
     expect(body).toMatchObject({ message: 'Entity registered successfully' })
     expect(body.data.id).toBeTruthy()
 
-    id = response.json().data.id
+    entityId = response.json().data.id
   })
 
   it('should get an entity', async () => {
     const response = await app.inject({
       method: 'GET',
-      url: `/api/entity/${id}`
+      url: `/api/entity/${entityId}`
     })
 
     expect(response.statusCode).toBe(200)
     const body = response.json()
     expect(body).toMatchObject({ message: 'Entity retrieved successfully' })
+  })
+
+  it('should update an description of an existing entity successfully', async () => {
+    const response = await app.inject({
+      method: 'PUT',
+      url: `/api/entity/${entityId}`,
+      body: { description: 'Updated description' }
+    })
+
+    expect(response.statusCode).toBe(200)
+    const body = response.json()
+    expect(body).toMatchObject({ message: 'Entity updated successfully' })
+  })
+
+  it('should retrieve the description updated from an entity successfully', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/api/entity/${entityId}`
+    })
+
+    expect(response.statusCode).toBe(200)
+    const body = response.json()
+    expect(body).toMatchObject({ message: 'Entity retrieved successfully' })
+    expect(body.data.description).toBe('Updated description')
   })
 })
