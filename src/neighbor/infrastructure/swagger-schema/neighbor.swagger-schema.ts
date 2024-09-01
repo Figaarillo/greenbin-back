9b366d36f38d34ec3a16f086540152e04b305b0d
Figaarillo <axel.leonardi.22@gmail.com>
2024-09-01 19:21:46 -0300

    docs: create swagger doc for neighbor

---
+++ src/neighbor/infrastructure/swagger-schema/neighbor.swagger-schema.ts
@@ -0,0 +1,77 @@
+export const registerSwaggerSchema = {
+  description: 'This is an endpoint that register a new neighbor.',
+  tags: ['Neighbor'],
+  body: {
+    type: 'object',
+    properties: {
+      firstname: { type: 'string', description: 'Firstname of the neighbor.' },
+      lastname: { type: 'string', description: 'Lastname of the neighbor.' },
+      username: { type: 'string', description: 'Username of the neighbor.' },
+      email: { type: 'string', description: 'Email of the neighbor.' },
+      password: { type: 'string', description: 'Password of the neighbor.' },
+      dni: { type: 'number', description: 'DNI of the neighbor.' },
+      phoneNumber: { type: 'string', description: 'Phone number of the neighbor.' },
+      birthdate: { type: 'string', format: 'date-time', description: 'Birthdate of the neighbor.' }
+    },
+    required: ['firstname', 'lastname', 'username', 'email', 'password', 'dni', 'phoneNumber', 'birthdate']
+  },
+  response: {
+    201: {
+      description: 'Neighbor registered successfully',
+      type: 'object',
+      properties: {
+        status: { type: 'number' },
+        message: { type: 'string' },
+        data: {
+          type: 'object',
+          properties: {
+            id: { type: 'string', format: 'uuid', description: 'Unique identifier for the neighbor.' }
+          },
+          required: ['id']
+        }
+      },
+      required: ['status', 'message', 'data']
+    }
+  }
+}
+
+export const updateSwaggerSchema = {
+  description: 'This is an endpoint that updates a neighbor by ID.',
+  tags: ['Neighbor'],
+  params: {
+    type: 'object',
+    properties: {
+      id: { type: 'string', format: 'uuid', description: 'Unique identifier for the neighbor.' }
+    },
+    required: ['id']
+  },
+  body: {
+    type: 'object',
+    properties: {
+      firstname: { type: 'string', description: 'Firstname of the neighbor.' },
+      lastname: { type: 'string', description: 'Lastname of the neighbor.' },
+      username: { type: 'string', description: 'Username of the neighbor.' },
+      email: { type: 'string', description: 'Email of the neighbor.' },
+      phoneNumber: { type: 'string', description: 'Phone number of the neighbor.' }
+    },
+    required: ['firstname', 'lastname', 'email', 'username', 'phoneNumber']
+  },
+  response: {
+    200: {
+      description: 'Neighbor updated successfully',
+      type: 'object',
+      properties: {
+        status: { type: 'number' },
+        message: { type: 'string' },
+        data: {
+          type: 'object',
+          properties: {
+            id: { type: 'string', format: 'uuid', description: 'Unique identifier for the neigbhor.' }
+          },
+          required: ['id']
+        }
+      },
+      required: ['status', 'message', 'data']
+    }
+  }
+}
