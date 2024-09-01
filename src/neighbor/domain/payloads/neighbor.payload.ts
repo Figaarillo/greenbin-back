c8d6ab568c8cd7a6652e8f3d6eec480813f0e448
Figaarillo <axel.leonardi.22@gmail.com>
2024-09-01 15:59:36 -0300

    feat: create neighbor paylaod

---
+++ src/neighbor/domain/payloads/neighbor.payload.ts
@@ -0,0 +1,11 @@
+interface NeighborPayload {
+  firstname: string
+  lastname: string
+  username: string
+  email: string
+  password: string
+  dni: number
+  birthdate: Date
+}
+
+export default NeighborPayload
