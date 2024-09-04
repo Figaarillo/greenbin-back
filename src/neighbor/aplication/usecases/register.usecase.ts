4b124e038a3f0ace09a07a0196f59af95be76df4
Figaarillo <axel.leonardi.22@gmail.com>
2024-09-04 19:16:01 -0300

    chore: add validation to birhdate

--- src/neighbor/aplication/usecases/register.usecase.ts
+++ src/neighbor/aplication/usecases/register.usecase.ts
@@ -9,6 +9,19 @@ class RegisterNeighborUseCase {
   }
 
   async exec(payload: NeighborPayload): Promise<NeighborEntity> {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!payload.birthdate) {
+      throw new Error('Birthdate is required')
+    }
+
+    if (payload.birthdate.getFullYear() < 1900) {
+      throw new Error('Year must be 1900 or later')
+    }
+
+    if (payload.birthdate > new Date()) {
+      throw new Error('Date cannot be in the future')
+    }
+
     const newNeighbor = new NeighborEntity(payload)
 
     const neighbor = await this.repository.save(newNeighbor)
