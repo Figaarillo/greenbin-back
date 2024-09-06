7ec12f9f0b3328dc075d29be0026d3941015922b
Figaarillo <axel.leonardi.22@gmail.com>
2024-09-05 22:31:09 -0300

    feat: create entity to reward partnew

---
+++ src/reward-partner/domain/entities/reward-partner.entity.ts
@@ -0,0 +1,61 @@
+/* eslint-disable indent */
+import { BeforeCreate, BeforeUpdate, Entity, EventArgs, Property } from '@mikro-orm/postgresql'
+import { hash, verify } from 'argon2'
+import BaseEntity from '../../../shared/domain/entities/base.entity'
+import NeighborPayload from '../payloads/reward-partner.payload'
+import type NeighborUpdatePayload from '../payloads/reward-partner.update.payload'
+
+@Entity()
+class NeighborEntity extends BaseEntity {
+  @Property({ unique: true })
+  name: string
+
+  @Property()
+  address: string
+
+  @Property({ unique: true })
+  cuit: string
+
+  @Property({ unique: true })
+  email: string
+
+  @Property({ hidden: true, lazy: true })
+  password: string
+
+  constructor(payload: NeighborPayload) {
+    super()
+    this.name = payload.name
+    this.address = payload.address
+    this.cuit = payload.cuit
+    this.email = payload.email
+    this.password = payload.password
+  }
+
+  update(payload: NeighborUpdatePayload): void {
+    if (payload.name != null || payload.name !== '') {
+      this.name = payload.name
+    }
+    if (payload.address != null || payload.address !== '') {
+      this.address = payload.address
+    }
+    if (payload.email != null || payload.email !== '') {
+      this.email = payload.email
+    }
+  }
+
+  @BeforeCreate()
+  @BeforeUpdate()
+  async hashPassword(args: EventArgs<NeighborEntity>): Promise<void> {
+    const password = args.changeSet?.payload.password
+
+    if (password != null) {
+      this.password = await hash(password)
+    }
+  }
+
+  async verifyPassword(password: string): Promise<boolean> {
+    return await verify(this.password, password)
+  }
+}
+
+export default NeighborEntity
