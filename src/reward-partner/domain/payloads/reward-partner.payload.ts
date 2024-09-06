199c00e33681d5cf6ad327ca82c642a7cf2afab4
Figaarillo <axel.leonardi.22@gmail.com>
2024-09-05 22:20:50 -0300

    feat: create payloads to reward partnew

---
+++ src/reward-partner/domain/payloads/reward-partner.payload.ts
@@ -0,0 +1,9 @@
+interface NeighborPayload {
+  name: string
+  address: string
+  cuit: string
+  email: string
+  password: string
+}
+
+export default NeighborPayload
---
+++ src/reward-partner/domain/payloads/reward-partner.update.payload.ts
@@ -0,0 +1,9 @@
+interface NeighborUpdatePayload {
+  name: string
+  address: string
+  cuit: string
+  email: string
+  password: string
+}
+
+export default NeighborUpdatePayload
