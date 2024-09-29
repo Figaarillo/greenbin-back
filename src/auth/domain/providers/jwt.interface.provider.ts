9890d496c16a087cf4ffe550da2bd189cc2dd467
Figaarillo <axel.leonardi.22@gmail.com>
2024-09-29 19:10:18 -0300

    chore: rename providers folder

--- src/auth/domain/providers/jwt.interface.provider.ts
+++ src/auth/domain/providers/jwt.interface.provider.ts
@@ -1,6 +1,6 @@
 interface IJWTProvider {
   generateToken: (sub: string, payload: Record<string, string>, exp: string, key: string) => Promise<string>
-  verifyToken: (token: string, key: string) => Promise<any>
+  verifyToken: (token: string, key: string) => Promise<Record<string, string>>
 }
 
 export default IJWTProvider
