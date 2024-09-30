ea5ba8c0de6514d45b91df5edfe73cbecbb18cd4
Figaarillo <axel.leonardi.22@gmail.com>
2024-09-30 00:27:15 -0300

    feat: add routes to login and refresh token

--- src/neighbor/infrastructure/routes/neighbor.route.ts
+++ src/neighbor/infrastructure/routes/neighbor.route.ts
@@ -1,4 +1,4 @@
-import { type FastifyInstance, type FastifyRequest } from 'fastify'
+import { type FastifyReply, type FastifyInstance, type FastifyRequest } from 'fastify'
 import type NeighborHandler from '../handler/neighbor.handler'
 import { registerSwaggerSchema, updateSwaggerSchema } from '../swagger-schema/neighbor.swagger-schema'
 
@@ -9,14 +9,23 @@ class NeighborRoute {
   ) {}
 
   setupRoutes(): void {
+    this.router.post('/api/neighbor/auth/login', async (req, rep) => {
+      await this.handler.login(req, rep)
+    })
+    this.router.get('/api/neighbor/auth/refresh-token', {
+      preHandler: this.router.auth([this.router.validateRefreshToken]),
+      handler: async (req: FastifyRequest, res: FastifyReply) => {
+        await this.handler.refreshToken(req, res)
+      }
+    })
     this.router.post('/api/neighbor', { schema: registerSwaggerSchema }, async (req, res) => {
-      await this.handler.Register(req, res)
+      await this.handler.register(req, res)
     })
     this.router.put(
       '/api/neighbor/:id',
       { schema: updateSwaggerSchema },
       async (req: FastifyRequest<{ Params: { id: string } }>, res) => {
-        await this.handler.Update(req, res)
+        await this.handler.update(req, res)
       }
     )
   }
