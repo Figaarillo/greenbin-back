95962813d5333b7a94c6bda73e0bb6159dd8d15e
Figaarillo <axel.leonardi.22@gmail.com>
2024-09-30 01:05:10 -0300

    feat: add new method in repository

--- src/neighbor/domain/repositories/neighbor.repository.ts
+++ src/neighbor/domain/repositories/neighbor.repository.ts
@@ -5,6 +5,7 @@ import type NeighborUpdatePayload from '../payloads/neighbor.update.payload'
 interface NeighborRepository {
   list: (offset: number, limit: number) => Promise<Nullable<NeighborEntity[]>>
   find: (property: Record<string, string>) => Promise<Nullable<NeighborEntity>>
+  findWithPassword: (property: Record<string, string>) => Promise<Nullable<NeighborEntity>>
   save: (neighbor: NeighborEntity) => Promise<Nullable<NeighborEntity>>
   update: (id: string, payload: NeighborUpdatePayload) => Promise<Nullable<NeighborEntity>>
   delete: (id: string) => Promise<void>
