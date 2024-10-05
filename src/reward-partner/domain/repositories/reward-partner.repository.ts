eadf68732d3b513993f1f2cd66bc5f0e92616f9f
Figaarillo <axel.leonardi.22@gmail.com>
2024-10-05 08:45:34 -0300

    feat: add methods in repository

--- src/reward-partner/domain/repositories/reward-partner.repository.ts
+++ src/reward-partner/domain/repositories/reward-partner.repository.ts
@@ -3,8 +3,12 @@ import type RewardPartnerEntity from '../entities/reward-partner.entity'
 import type RewardPartnerUpdatePayload from '../payloads/reward-partner.update.payload'
 
 interface RewardPartnerRepository {
+  list: (offset: number, limit: number) => Promise<Nullable<RewardPartnerEntity[]>>
+  find: (property: Record<string, string>) => Promise<Nullable<RewardPartnerEntity>>
+  findWithPassword: (property: Record<string, string>) => Promise<Nullable<RewardPartnerEntity>>
   save: (rewardPartner: RewardPartnerEntity) => Promise<Nullable<RewardPartnerEntity>>
   update: (id: string, payload: RewardPartnerUpdatePayload) => Promise<Nullable<RewardPartnerEntity>>
+  delete: (id: string) => Promise<void>
 }
 
 export default RewardPartnerRepository
