06a82a52d36b260568b72081323f25729959b43c
Figaarillo <axel.leonardi.22@gmail.com>
2024-10-13 18:06:38 -0300

    feat: create green point repository

---
+++ src/green-point/domain/repositories/green-point.repository.ts
@@ -0,0 +1,14 @@
+import type Nullable from '../../../shared/domain/types/nullable.type'
+import type GreenPointEntity from '../entities/green-point.entity'
+import type GreenPointUpdatePayload from '../payloads/green-point.update.payload'
+
+interface GreenPointRepository {
+  list: (offset: number, limit: number) => Promise<Nullable<GreenPointEntity[]>>
+  find: (property: Record<string, string>) => Promise<Nullable<GreenPointEntity>>
+  findWithPassword: (property: Record<string, string>) => Promise<Nullable<GreenPointEntity>>
+  save: (greenPoint: GreenPointEntity) => Promise<Nullable<GreenPointEntity>>
+  update: (id: string, payload: GreenPointUpdatePayload) => Promise<Nullable<GreenPointEntity>>
+  delete: (id: string) => Promise<void>
+}
+
+export default GreenPointRepository
