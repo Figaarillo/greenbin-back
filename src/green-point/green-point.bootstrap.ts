55cfe2862e89cf20ce29861d15c6e8e6939ecca1
Figaarillo <axel.leonardi.22@gmail.com>
2024-10-14 00:18:37 -0300

    feat: add green point bootstrap

---
+++ src/green-point/green-point.bootstrap.ts
@@ -0,0 +1,16 @@
+import { type FastifyInstance } from 'fastify'
+import type GreenPointRepository from './domain/repositories/green-point.repository'
+import GreenPointHandler from './infrastructure/handler/green-point.handler'
+import GreenPointMikroORMRepository from './infrastructure/repositories/mikro-orm/green-point.mikroorm.repository'
+import GreenPointRoute from './infrastructure/routes/green-point.route'
+
+async function bootstrapGreenPoint(server: FastifyInstance): Promise<void> {
+  const repository: GreenPointRepository = new GreenPointMikroORMRepository()
+
+  const handler = new GreenPointHandler(repository)
+
+  const greenPointRoute = new GreenPointRoute(server, handler)
+  greenPointRoute.setupRoutes()
+}
+
+export default bootstrapGreenPoint
