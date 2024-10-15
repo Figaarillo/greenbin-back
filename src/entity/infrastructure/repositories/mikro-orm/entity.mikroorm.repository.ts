050906b78d87652578f0fc223f142efac15f25ba
Figaarillo <axel.leonardi.22@gmail.com>
2024-10-14 21:30:48 -0300

    refactor: refactor implementation of mikro orm repository
    fix bug when try to create a new entity

--- src/entity/infrastructure/repositories/mikro-orm/entity.mikroorm.repository.ts
+++ src/entity/infrastructure/repositories/mikro-orm/entity.mikroorm.repository.ts
@@ -1,50 +1,74 @@
-import { type EntityManager } from '@mikro-orm/postgresql'
-import { type Services } from '../../../../db'
+import { RequestContext } from '@mikro-orm/core'
 import type Nullable from '../../../../shared/domain/types/nullable.type'
 import EntityEntity from '../../../domain/entities/entity.entity'
 import type EntityRepository from '../../../domain/repositories/entity.repository'
 
 class EntityMikroORMRepository implements EntityRepository {
-  private readonly em: EntityManager
-
-  constructor(private readonly db: Services) {
-    this.em = this.db.em.fork()
-  }
-
   async list(offset: number, limit: number): Promise<Nullable<EntityEntity[]>> {
-    return await this.em.find(EntityEntity, {}, { limit, offset })
+    const em = RequestContext.getEntityManager()
+    if (em == null) {
+      throw new Error('No `EntityManager` found in RequestContext')
+    }
+
+    return await em.find(EntityEntity, {}, { limit, offset })
   }
 
   async find(property: Record<string, string>): Promise<Nullable<EntityEntity>> {
-    return await this.em.findOne(EntityEntity, property)
+    const em = RequestContext.getEntityManager()
+    if (em == null) {
+      throw new Error('No `EntityManager` found in RequestContext')
+    }
+
+    return await em.findOne(EntityEntity, property)
   }
 
   async findWithPassword(property: Record<string, string>): Promise<Nullable<EntityEntity>> {
-    return await this.em.findOne(EntityEntity, property, { populate: ['password'] })
+    const em = RequestContext.getEntityManager()
+    if (em == null) {
+      throw new Error('No `EntityManager` found in RequestContext')
+    }
+
+    return await em.findOne(EntityEntity, property, { populate: ['password'] })
   }
 
   async save(entity: EntityEntity): Promise<Nullable<EntityEntity>> {
-    const newEntity = this.em.create(EntityEntity, entity)
-    await this.em.persist(newEntity).flush()
+    const em = RequestContext.getEntityManager()
+    if (em == null) {
+      throw new Error('No `EntityManager` found in RequestContext')
+    }
 
-    return newEntity
+    await em.persist(entity).flush()
+
+    return entity
   }
 
   async update(id: string, description: string): Promise<Nullable<EntityEntity>> {
-    const entity = this.em.getReference(EntityEntity, id)
+    const em = RequestContext.getEntityManager()
+    if (em == null) {
+      throw new Error('No `EntityManager` found in RequestContext')
+    }
+
+    const entity = em.getReference(EntityEntity, id)
     if (entity == null) return null
 
     entity.update(description)
-    await this.em.flush()
+    await em.flush()
 
     return entity
   }
 
   async delete(id: string): Promise<void> {
-    const entity = this.em.getReference(EntityEntity, id)
-    if (entity == null) throw new Error('Entity not found')
+    const em = RequestContext.getEntityManager()
+    if (em == null) {
+      throw new Error('No `EntityManager` found in RequestContext')
+    }
 
-    await this.em.remove(entity).flush()
+    const entity = em.getReference(EntityEntity, id)
+    if (entity == null) {
+      throw new Error('Entity not found')
+    }
+
+    await em.remove(entity).flush()
   }
 }
 
