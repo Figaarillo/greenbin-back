a0caa171fc3f720e0ee3dc98f4b1f961b472d9fd
Figaarillo <axel.leonardi.22@gmail.com>
2024-10-15 23:13:00 -0300

    feat: add access token validation

--- src/neighbor/infrastructure/routes/neighbor.route.ts
+++ src/neighbor/infrastructure/routes/neighbor.route.ts
@@ -9,22 +9,28 @@ class NeighborRoute {
   ) {}
 
   setupRoutes(): void {
-    this.server.get('/api/neighbor', async (req: FastifyRequest<{ Querystring: Record<string, string> }>, rep) => {
-      await this.handler.list(req, rep)
+    this.server.get('/api/neighbor', {
+      preHandler: this.server.auth([this.server.validateAccessToken]),
+      handler: async (req: FastifyRequest<{ Querystring: Record<string, string> }>, rep) => {
+        await this.handler.list(req, rep)
+      }
     })
-    this.server.get('/api/neighbor/:id', async (req: FastifyRequest<{ Params: { id: string } }>, rep) => {
-      await this.handler.findById(req, rep)
+    this.server.get('/api/neighbor/:id', {
+      preHandler: this.server.auth([this.server.validateAccessToken]),
+      handler: async (req: FastifyRequest<{ Params: Record<string, string> }>, rep: FastifyReply) => {
+        await this.handler.findById(req, rep)
+      }
     })
     this.server.post('/api/neighbor', { schema: registerSwaggerSchema }, async (req, rep) => {
       await this.handler.register(req, rep)
     })
-    this.server.put(
-      '/api/neighbor/:id',
-      { schema: updateSwaggerSchema },
-      async (req: FastifyRequest<{ Params: { id: string } }>, rep) => {
+    this.server.put('/api/neighbor/:id', {
+      schema: updateSwaggerSchema,
+      preHandler: this.server.auth([this.server.validateAccessToken]),
+      handler: async (req: FastifyRequest<{ Params: { id: string } }>, rep: FastifyReply) => {
         await this.handler.update(req, rep)
       }
-    )
+    })
     this.server.post('/api/neighbor/auth/login', async (req, rep) => {
       await this.handler.login(req, rep)
     })
