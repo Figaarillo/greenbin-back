3c269c9dd63ac6bae221d493370f1b9602f0b2fe
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-02 11:13:39 -0300

    feat: create waste entity

---
+++ src/waste/domain/entities/waste.entity.ts
@@ -0,0 +1,40 @@
+/* eslint-disable indent */
+import { Collection, Entity, ManyToMany, ManyToOne, Property } from '@mikro-orm/core'
+import BaseEntity from '../../../shared/domain/entities/base.entity'
+import WasteCategoryEntity from '../../../waste-category/domain/entities/waste-category.entity'
+import WastePayload from '../payloads/waste.payload'
+import type NeighborEntity from '../../../neighbor/domain/entities/neighbor.entity'
+
+@Entity()
+class WasteEntity extends BaseEntity {
+  @ManyToOne()
+  category: WasteCategoryEntity
+
+  @Property()
+  points: number
+
+  @Property()
+  weight: number
+
+  @Property()
+  pointsPerWeight: number
+
+  @ManyToMany({})
+  neighbors = new Collection<NeighborEntity>(this)
+
+  constructor(payload: WastePayload) {
+    super()
+    this.category = payload.category
+    this.weight = payload.weight
+    this.points = 0
+    this.pointsPerWeight = this.category.pointsPerWeight
+  }
+
+  calculatePoints(): number {
+    const points = this.pointsPerWeight * this.weight
+    this.points = points
+    return points
+  }
+}
+
+export default WasteEntity
