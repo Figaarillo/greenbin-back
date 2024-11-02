d30cd7262fcddc96bf2d3da97ca45c167c75c6cf
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-02 11:49:58 -0300

    feat: create waste routes

---
+++ src/waste/infrastructure/routes/waste.route.ts
@@ -0,0 +1,25 @@
+import { type FastifyInstance, type FastifyRequest } from 'fastify'
+import { findByIdSwaggerSchema } from '../../../entity/infrastructure/swagger-schemas/entity.swagger-schema'
+import type WasteHandler from '../handlers/waste.handler'
+
+class WasteRoute {
+  constructor(
+    private readonly server: FastifyInstance,
+    private readonly handler: WasteHandler
+  ) {}
+
+  setupRoutes(): void {
+    this.server.get(
+      '/api/waste/:id',
+      { schema: findByIdSwaggerSchema },
+      async (req: FastifyRequest<{ Params: { id: string } }>, res) => {
+        await this.handler.findByID(req, res)
+      }
+    )
+    this.server.post('/api/waste', async (req, res) => {
+      await this.handler.register(req, res)
+    })
+  }
+}
+
+export default WasteRoute
