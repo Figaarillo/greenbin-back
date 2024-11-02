8c2a6ab0447a224cdb1372a52d26f74b1ea640af
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-02 18:48:17 -0300

    refactor(waste): add find category to can get points per weigh from category entity

--- src/waste/application/usecases/register.usecase.ts
+++ src/waste/application/usecases/register.usecase.ts
@@ -1,3 +1,4 @@
+import ErrorCategoryNotFound from '../../../waste-category/domain/errors/category-not-found.error'
 import WasteEntity from '../../domain/entities/waste.entity'
 import ErrorCannotSaveWaste from '../../domain/errors/cannot-save-waste.error'
 import type WastePayload from '../../domain/payloads/waste.payload'
@@ -7,8 +8,15 @@ class RegisterWasteUseCase {
   constructor(private readonly repository: WasteRepository) {}
 
   async exec(payload: WastePayload): Promise<WasteEntity> {
-    const newWaste = new WasteEntity(payload)
+    const categoryId = payload.category as unknown as string
+    const category = await this.repository.findCategory({ id: categoryId })
+    if (category == null) {
+      throw new ErrorCategoryNotFound(categoryId)
+    }
 
+    const newWaste = new WasteEntity({ ...payload, pointsPerWeight: category.pointsPerWeight })
+
+    console.log({ newWaste, where: 'in usecase' })
     const waste = await this.repository.save(newWaste)
     if (waste == null) {
       throw new ErrorCannotSaveWaste()
