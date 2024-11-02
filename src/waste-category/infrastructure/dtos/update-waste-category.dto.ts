58ef83ecf6f33da1b364851ccc2f8fdb79418661
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-02 17:20:21 -0300

    chore(waste-category): correct update DTO

--- src/waste-category/infrastructure/dtos/update-waste-category.dto.ts
+++ src/waste-category/infrastructure/dtos/update-waste-category.dto.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod'
 import { descriptionDTO, nameDTO } from './dto-types/dto-types'
+import { pointsPerWeightDTO } from '../../../shared/infrastructure/dto-types/dto-types'
 
 const UpdateWasteCategoryDTO = z.object({
-  name: nameDTO,
-  description: descriptionDTO
+  name: nameDTO.optional(),
+  pointsPerWeight: pointsPerWeightDTO.optional(),
+  description: descriptionDTO.optional()
 })
 
 export default UpdateWasteCategoryDTO
