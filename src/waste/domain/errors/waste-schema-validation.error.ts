1b54acb6dca62be16fde2fcfcd46d037cb7c0315
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-02 11:57:34 -0300

    chore(waste): delete shema validation error

--- src/waste/domain/errors/waste-schema-validation.error.ts
+++
@@ -1,8 +0,0 @@
-class ErrorWasteSchemaValidation extends Error {
-  constructor(message: string) {
-    super(message)
-    this.name = ErrorWasteSchemaValidation.name
-  }
-}
-
-export default ErrorWasteSchemaValidation
