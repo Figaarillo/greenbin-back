453e9af9c02f409309576fca5396cb27f03edf6d
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-03 15:30:11 -0300

    refactor(waste-transaction): add functions to validata that IDs exits

--- src/waste-transaction/application/usecases/register.usecase.ts
+++ src/waste-transaction/application/usecases/register.usecase.ts
@@ -1,3 +1,6 @@
+import ErrorGreenPointNotFound from '../../../green-point/domain/errors/green-point-not-found.error'
+import ErrorNeighborNotFound from '../../../neighbor/domain/errors/neighbor-not-found.error'
+import ErrorResponsibleNotFound from '../../../responsible/domain/errors/responsible-not-found.error'
 import WasteTransactionEntity from '../../domain/entities/waste-transaction.entity'
 import ErrorCannotSaveWasteTransaction from '../../domain/errors/cannot-save-waste-transaction.error'
 import type WasteTransactionPayload from '../../domain/payloads/waste-transaction.payload'
@@ -7,7 +10,30 @@ class RegisterWasteTransactionUseCase {
   constructor(private readonly repository: WasteTransactionRepository) {}
 
   async exec(payload: WasteTransactionPayload): Promise<any> {
+    console.log({ payload })
+
+    const idResponsible: string = payload.responsible as unknown as string
+    const idNeighbor: string = payload.neighbor as unknown as string
+    const idGreenPoint: string = payload.greenPoint as unknown as string
+
+    const responsible = await this.repository.findResponsible({ id: idResponsible })
+    if (responsible == null) {
+      throw new ErrorResponsibleNotFound(idResponsible)
+    }
+
+    const neighbor = await this.repository.findNeighbor({ id: idNeighbor })
+    if (neighbor == null) {
+      throw new ErrorNeighborNotFound(idNeighbor)
+    }
+
+    const greenPoint = await this.repository.fidnGreenPoint({ id: idGreenPoint })
+    if (greenPoint == null) {
+      throw new ErrorGreenPointNotFound(idGreenPoint)
+    }
+
     const newTransaction = new WasteTransactionEntity(payload)
+
+    console.log({ newTransaction })
 
     const wasteTransaction = await this.repository.save(newTransaction)
     if (wasteTransaction == null) {
