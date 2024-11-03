0e5415b344dc8aafc39bbae217c118d6e3c02acb
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-03 15:26:32 -0300

    refactor(waste-transaction): update repository to add more methods

--- src/waste-transaction/domain/repositories/waste-transaction.repository.ts
+++ src/waste-transaction/domain/repositories/waste-transaction.repository.ts
@@ -1,9 +1,15 @@
+import type GreenPointEntity from '../../../green-point/domain/entities/green-point.entity'
+import type NeighborEntity from '../../../neighbor/domain/entities/neighbor.entity'
+import type ResponsibleEntity from '../../../responsible/domain/entities/responsible.entity'
 import type Nullable from '../../../shared/domain/types/nullable.type'
 import type WasteTransactionEntity from '../entities/waste-transaction.entity'
 
 interface WasteTransactionRepository {
   list: (offset?: number, limit?: number) => Promise<Nullable<WasteTransactionEntity[]>>
   find: (property: Record<string, string>) => Promise<Nullable<WasteTransactionEntity>>
+  findResponsible: (property: Record<string, string>) => Promise<Nullable<ResponsibleEntity>>
+  findNeighbor: (property: Record<string, string>) => Promise<Nullable<NeighborEntity>>
+  fidnGreenPoint: (property: Record<string, string>) => Promise<Nullable<GreenPointEntity>>
   save: (transaction: WasteTransactionEntity) => Promise<Nullable<WasteTransactionEntity>>
 }
 
--- src/waste-transaction/infrastructure/repositories/mikro-orm/waste-transaction.mikroorm.repository.ts
+++ src/waste-transaction/infrastructure/repositories/mikro-orm/waste-transaction.mikroorm.repository.ts
@@ -3,6 +3,9 @@ import ErrorEntityManagerNotFound from '../../../../shared/domain/errors/entity-
 import type Nullable from '../../../../shared/domain/types/nullable.type'
 import WasteTransactionEntity from '../../../domain/entities/waste-transaction.entity'
 import type WasteTransactionRepository from '../../../domain/repositories/waste-transaction.repository'
+import ResponsibleEntity from '../../../../responsible/domain/entities/responsible.entity'
+import NeighborEntity from '../../../../neighbor/domain/entities/neighbor.entity'
+import GreenPointEntity from '../../../../green-point/domain/entities/green-point.entity'
 
 class WasteTransactionMikroORMRepository implements WasteTransactionRepository {
   list!: (offset?: number | undefined, limit?: number | undefined) => Promise<Nullable<WasteTransactionEntity[]>>
@@ -10,6 +13,21 @@ class WasteTransactionMikroORMRepository implements WasteTransactionRepository {
   async find(property: Record<string, string>): Promise<Nullable<WasteTransactionEntity>> {
     const em = this.getEntityManager()
     return await em.findOne(WasteTransactionEntity, property)
+  }
+
+  async findResponsible(property: Record<string, string>): Promise<Nullable<ResponsibleEntity>> {
+    const em = this.getEntityManager()
+    return await em.findOne(ResponsibleEntity, property)
+  }
+
+  async findNeighbor(property: Record<string, string>): Promise<Nullable<NeighborEntity>> {
+    const em = this.getEntityManager()
+    return await em.findOne(NeighborEntity, property)
+  }
+
+  async fidnGreenPoint(property: Record<string, string>): Promise<Nullable<GreenPointEntity>> {
+    const em = this.getEntityManager()
+    return await em.findOne(GreenPointEntity, property)
   }
 
   async save(transaction: WasteTransactionEntity): Promise<Nullable<WasteTransactionEntity>> {
