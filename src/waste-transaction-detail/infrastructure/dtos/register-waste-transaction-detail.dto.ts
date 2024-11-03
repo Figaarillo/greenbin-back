085ca591d3e4b1bccf50e874753e7308d261e2c1
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-03 15:41:11 -0300

    chore(waste-transaction-detail): update register dto

--- src/waste-transaction-detail/infrastructure/dtos/register-waste-transaction-detail.dto.ts
+++ src/waste-transaction-detail/infrastructure/dtos/register-waste-transaction-detail.dto.ts
@@ -2,7 +2,8 @@ import { z } from 'zod'
 import { idDTO } from '../../../shared/infrastructure/dto-types/dto-types'
 
 const RegisterWasteTransactionDetailDTO = z.object({
-  waste: idDTO
+  waste: idDTO,
+  transaction: idDTO
 })
 
 export default RegisterWasteTransactionDetailDTO
