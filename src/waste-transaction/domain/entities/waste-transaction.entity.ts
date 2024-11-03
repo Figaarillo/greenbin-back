1172d69fd28c26eee667db22afde6cc07de89ea2
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-03 15:24:11 -0300

    refactor(waste-transaction): enhance entity
    now the transaction details must be added once they have been created
    through the addTransactionDetail function.

--- src/waste-transaction/domain/entities/waste-transaction.entity.ts
+++ src/waste-transaction/domain/entities/waste-transaction.entity.ts
@@ -1,12 +1,13 @@
 /* eslint-disable indent */
-import { Collection, ManyToOne, OneToMany, Property } from '@mikro-orm/postgresql'
+import { Collection, Entity, ManyToOne, OneToMany, Property } from '@mikro-orm/postgresql'
 import GreenPointEntity from '../../../green-point/domain/entities/green-point.entity'
 import NeighborEntity from '../../../neighbor/domain/entities/neighbor.entity'
 import ResponsibleEntity from '../../../responsible/domain/entities/responsible.entity'
 import BaseEntity from '../../../shared/domain/entities/base.entity'
 import WasteTransactionDetailEntity from '../../../waste-transaction-detail/domain/entities/waste-transaction-detail.entity'
-import type WasteTransactionPayload from '../payloads/waste-transaction.payload'
+import WasteTransactionPayload from '../payloads/waste-transaction.payload'
 
+@Entity({ tableName: 'wastes_transactions' })
 class WasteTransactionEntity extends BaseEntity {
   @ManyToOne()
   responsible: ResponsibleEntity
@@ -21,30 +22,27 @@ class WasteTransactionEntity extends BaseEntity {
   transactionDetails = new Collection<WasteTransactionDetailEntity>(this)
 
   @Property()
-  totalPoints: number
+  totalPoints: number = 0
 
   @Property()
-  date: Date
+  date: Date = new Date()
 
   constructor(payload: WasteTransactionPayload) {
     super()
     this.responsible = payload.responsible
     this.neighbor = payload.neighbor
     this.greenPoint = payload.greenPoint
-    this.addTransactionDetail(payload.transactionDetails)
-    this.totalPoints = 0
-    this.date = new Date()
   }
 
-  private addTransactionDetail(details: WasteTransactionDetailEntity[]): void {
-    // eslint-disable-next-line array-callback-return
-    details.map(detail => {
-      this.transactionDetails.add(detail)
-    })
+  addTransactionDetail(detail: WasteTransactionDetailEntity): void {
+    this.transactionDetails.add(detail)
+    this.totalPoints += detail.points
   }
 
-  caculateTotalPoints(): void {
-    this.transactionDetails.map(detail => (this.totalPoints += detail.points))
+  calculateTotalPoints(): number {
+    this.totalPoints = this.transactionDetails.getItems().reduce((sum, detail) => sum + detail.points, 0)
+
+    return this.totalPoints
   }
 }
 
