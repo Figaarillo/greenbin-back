9fde00b116dd9c9dc1b13e5214f34155ddcf9447
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-04 00:23:05 -0300

    chore(waste-transaction): remve find from register use case

--- src/waste-transaction-detail/application/usecases/register.usecase.ts
+++ src/waste-transaction-detail/application/usecases/register.usecase.ts
@@ -1,4 +1,3 @@
-import ErrorWasteTransactionNotFound from '../../../waste-transaction/domain/errors/waste-transaction-not-found.error'
 import WasteTransactionDetailEntity from '../../domain/entities/waste-transaction-detail.entity'
 import ErrorCannotSaveWasteTransactionDetail from '../../domain/errors/cannot-save-waste-transaction-detail.error'
 import type WasteTransactionDetailPayload from '../../domain/payloads/waste-transaction-detail.payload'
@@ -9,12 +8,6 @@ class RegisterWasteTransactionDetailUseCase {
 
   async exec(paylaod: WasteTransactionDetailPayload): Promise<WasteTransactionDetailEntity> {
     const newTransactionDetail = new WasteTransactionDetailEntity(paylaod)
-
-    const idTransaction: string = paylaod.transaction as unknown as string
-    const transaction = await this.repository.findWasteTransaction({ id: idTransaction })
-    if (transaction == null) {
-      throw new ErrorWasteTransactionNotFound()
-    }
 
     const transactionDetail = await this.repository.save(newTransactionDetail)
     if (transactionDetail == null) {
