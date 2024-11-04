4c4411311e4dc8b3c6342ceaba2470502c8e83c9
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-04 00:21:23 -0300

    refactor(waste-transaction): update bootstrap

--- src/waste-transaction/waste-transaction.bootstrap.ts
+++ src/waste-transaction/waste-transaction.bootstrap.ts
@@ -1,13 +1,27 @@
 import { type FastifyInstance } from 'fastify'
+import type NeighborRepository from '../neighbor/domain/repositories/neighbor.repository'
+import NeighborMikroORMRepository from '../neighbor/infrastructure/repositories/mikro-orm/neighbor.mikroorm.repository'
+import type WasteTransactionDetailRepository from '../waste-transaction-detail/domain/repositories/waste-transaction-detail.repository'
+import WasteTransactionDetailMikroORMRepository from '../waste-transaction-detail/infrastructure/repositories/mikro-orm/waste-transaction-detail.mikroorm.repository'
+import type WasteRepository from '../waste/domain/repositories/waste.repository'
+import WasteMikroORMRepository from '../waste/infrastructure/repositories/mikro-orm/waste.mikroorm.repository'
 import type WasteTransactionRepository from './domain/repositories/waste-transaction.repository'
 import WasteTransactionHandler from './infrastructure/handlers/waste-transaction.handler'
 import WasteTransactionMikroORMRepository from './infrastructure/repositories/mikro-orm/waste-transaction.mikroorm.repository'
 import WasteTransactionRoute from './infrastructure/routes/waste-transaction.route'
 
 async function bootstrapWasteTransaction(router: FastifyInstance): Promise<void> {
-  const repository: WasteTransactionRepository = new WasteTransactionMikroORMRepository()
+  const transactionRepository: WasteTransactionRepository = new WasteTransactionMikroORMRepository()
+  const transactionDetailRepository: WasteTransactionDetailRepository = new WasteTransactionDetailMikroORMRepository()
+  const wasteRepository: WasteRepository = new WasteMikroORMRepository()
+  const neighborRepository: NeighborRepository = new NeighborMikroORMRepository()
 
-  const handler = new WasteTransactionHandler(repository)
+  const handler = new WasteTransactionHandler(
+    transactionDetailRepository,
+    transactionRepository,
+    wasteRepository,
+    neighborRepository
+  )
 
   const routes = new WasteTransactionRoute(router, handler)
   routes.setupRoutes()
