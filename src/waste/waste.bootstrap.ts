8392a29f3841a6ff15fe5fe1ae4e7710a0c01a41
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-17 19:13:52 -0300

    refactor: update bootstraps

--- src/waste/waste.bootstrap.ts
+++ src/waste/waste.bootstrap.ts
@@ -1,13 +1,16 @@
 import { type FastifyInstance } from 'fastify'
+import type WasteCategoryRepository from '../waste-category/domain/repositories/waste-category.repository'
+import CategoryMikroORMRepository from '../waste-category/infrastructure/repositories/mikro-orm/waste-category.mikroorm.repository'
 import type WasteRepository from './domain/repositories/waste.repository'
 import WasteHandler from './infrastructure/handlers/waste.handler'
 import WasteMikroORMRepository from './infrastructure/repositories/mikro-orm/waste.mikroorm.repository'
 import WasteRoute from './infrastructure/routes/waste.route'
 
 async function bootstrapWaste(router: FastifyInstance): Promise<void> {
-  const repository: WasteRepository = new WasteMikroORMRepository()
+  const wasteRepository: WasteRepository = new WasteMikroORMRepository()
+  const categoryRepository: WasteCategoryRepository = new CategoryMikroORMRepository()
 
-  const handler = new WasteHandler(repository)
+  const handler = new WasteHandler(wasteRepository, categoryRepository)
 
   const routes = new WasteRoute(router, handler)
   routes.setupRoutes()
