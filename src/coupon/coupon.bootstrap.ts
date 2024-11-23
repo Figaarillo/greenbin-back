24a85968d33ab6bbc65fffdb0504cf23f018424d
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-23 00:42:09 -0300

    chore(coupon): fix bootstrapt function

--- src/coupon/coupon.bootstrap.ts
+++ src/coupon/coupon.bootstrap.ts
@@ -1,9 +1,10 @@
 import { type FastifyInstance } from 'fastify'
+import type RewardPartnerRepository from '../reward-partner/domain/repositories/reward-partner.repository'
 import RewardPartnerMikroORMRepository from '../reward-partner/infrastructure/repositories/mikro-orm/reward-partner.mikroorm.repository'
 import type CouponRepository from './domain/repositories/coupon.repository'
 import CouponHandler from './infrastructure/handlers/coupon.handler'
+import CouponMikroORMRepository from './infrastructure/repositories/mikro-orm/coupon.mikroorm.repository'
 import CouponRoute from './infrastructure/routes/coupon.route'
-import type RewardPartnerRepository from '../reward-partner/domain/repositories/reward-partner.repository'
 
 async function bootstrapCoupon(router: FastifyInstance): Promise<void> {
   const couponRepository: CouponRepository = new CouponMikroORMRepository()
