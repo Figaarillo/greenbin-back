e704aa635aaeb581b82c8679d51e3e5464c624ab
Figaarillo <axel.leonardi.22@gmail.com>
2024-11-25 03:29:14 -0300

    chore(coupon-transaction): create dtos and middleware

---
+++ src/coupon-transaction/infrastructure/middlewares/zod-schema-validator.middleware.ts
@@ -0,0 +1,24 @@
+import { ZodError, type ZodType } from 'zod'
+import handleZodError from '../../../shared/utils/hanlde-zod-error.util'
+import type CouponTransactionPayload from '../../domain/payloads/coupon-transaction.payload'
+import type ExtendPayload from '../../../shared/domain/types/ext-payload.type'
+import type RedeemCouponPayload from '../../domain/payloads/redeem-coupon.payload'
+
+class CouponTransactionSchemaValidator<TDTOSchema> {
+  constructor(
+    private readonly schema: ZodType<TDTOSchema>,
+    private readonly payload: Partial<ExtendPayload<CouponTransactionPayload | RedeemCouponPayload>>
+  ) {}
+
+  exec(): TDTOSchema {
+    try {
+      return this.schema.parse(this.payload)
+    } catch (error) {
+      if (error instanceof ZodError) handleZodError(error)
+
+      throw error
+    }
+  }
+}
+
+export default CouponTransactionSchemaValidator
