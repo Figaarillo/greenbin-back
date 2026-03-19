ba94e6b259c933a8c93d1c80889074ecff0f4fb3
Licha <mattaliaenzojesus@gmail.com>
2026-03-19 21:24:28 +0000

    feat: migrations and property waste category

--- src/migrations/Migration20260314164723.ts
+++ src/migrations/Migration20260314164723.ts
@@ -3,15 +3,11 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20260314164723 extends Migration {
 
   async up(): Promise<void> {
-    this.addSql('alter table "neighbors" drop column "deleted_at";');
-
-    this.addSql('alter table "neighbors" add column "is_active" boolean not null default true;');
+    this.addSql('alter table "neighbors" add column if not exists "is_active" boolean not null default true;');
   }
 
   async down(): Promise<void> {
-    this.addSql('alter table "neighbors" drop column "is_active";');
-
-    this.addSql('alter table "neighbors" add column "deleted_at" timestamptz null;');
+    this.addSql('alter table "neighbors" drop column if exists "is_active";');
   }
 
 }
